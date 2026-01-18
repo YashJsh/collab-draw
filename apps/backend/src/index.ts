fe21e41d82b44ecc00ca74107b1b4817aa976983
yash <yaxj28@gmail.com>
2026-01-18 19:59:34 +0530

    wssClientConnectsSuccessfully

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,15 @@
 import http from "http";
 import {WebSocketServer}from "ws";
 import {app} from "./app.js"
+import { setupWebSocketHandlers } from "./socket/socket.js";
 
 const server = http.createServer(app);
 
 const wss = new WebSocketServer ({ server });
+setupWebSocketHandlers(wss);
  
-export { wss };
-
 server.listen(3001, ()=>{
     console.log("Server is listening on port 3001");
 });
+
+export { wss };
--- apps/backend/src/socket/socket.ts
+++ apps/backend/src/socket/socket.ts
@@ -1,20 +1,36 @@
+import type { WebSocketServer } from "ws";
 import {wss} from "../index.js";
 import { verifyToken } from "../utils/tokenManagment.js";
 
-wss.on("connection", (ws, request)=>{
-    const url = request.url;
-    if (!url){
-        return;
-    }
-    const queryParams = new URLSearchParams(url.split("?")[1]);
-    const token = queryParams.get("token") || "";
-    const decoded = verifyToken(token);
-    if (!decoded || !decoded.userId){
-        ws.close();
-        return;
-    }
-    ws.on("message", (data)=>{
-        ws.send("PONG");
+export function setupWebSocketHandlers(wss : WebSocketServer) {
+    wss.on("connection", (ws, request)=>{
+    // const url = request.url;
+    // if (!url){
+    //     return;
+    // }
+    // const queryParams = new URLSearchParams(url.split("?")[1]);
+    // const token = queryParams.get("token") || "";
+    // const decoded = verifyToken(token);
+    // if (!decoded || !decoded.userId){
+    //     ws.close();
+    //     return;
+    // };
+    console.log("New client connected to WebSocket");
+    ws.on("message", (data : Buffer)=>{
+        console.log("Received:", data);
+        const dataStr = data.toString();
+        console.log(data);
+        const objectData = JSON.parse(dataStr);
+        console.log(objectData);
+        if (objectData.type === "ping"){
+            ws.send(JSON.stringify({
+                type : "pong",
+                message : "Welcome to excali-draw websocket server"
+            }));
+        }
     });
 })
+}
+
+
 
